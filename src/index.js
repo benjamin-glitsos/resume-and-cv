e0eadee1459efec52facbf914244fa9b32b58b30
Benjamin Glitsos <bengyup@gmail.com>
2021-01-08 21:52:54 +1100

    Mustache interpolation is now working correctly

--- src/index.js
+++ src/index.js
@@ -5,51 +5,52 @@ const chalk = require("chalk");
 const match = require("switchcase");
 const { STATE } = require("./state");
 const { metadata } = require("./metadata");
-const { quote, logNumber, argSettingOrDefault } = require("./utilities");
-
-const data = (() => {
-    const commandLineArguments = process.argv.slice(2);
-
-    const { documentTypes, jobTypes } = metadata.selections;
-
-    const documentType = argSettingOrDefault(
-        commandLineArguments,
-        documentTypes,
-        documentTypes.RESUME
-    );
-
-    const jobType = argSettingOrDefault(
-        commandLineArguments,
-        jobTypes,
-        jobTypes.BA
-    );
-
-    const output = {};
-
-    output.documentNameText = match({
-        [documentTypes.CV]: "CV",
-        default: "Resume"
-    })(documentType);
-
-    output.documentNameLatex = match({
-        [documentTypes.CV]: "CURRICULUM VITAE",
-        default: String.raw`R\'{E}SUM\'{E}`
-    })(documentType);
-
-    return output;
-})();
+const { identity, logNumber, argSettingOrDefault } = require("./utilities");
 
 try {
-    const outputPath = `./output/${data.documentNameText} of Benjamin Glitsos.tex`;
+    Mustache.escape = identity;
 
-    const template = fs.readFileSync(
-        "./resources/template.mustache.tex",
-        "utf8"
-    );
+    const data = (() => {
+        const commandLineArguments = process.argv.slice(2);
+
+        const { documentTypes, jobTypes } = metadata.selections;
+
+        const documentType = argSettingOrDefault(
+            commandLineArguments,
+            documentTypes,
+            documentTypes.RESUME
+        );
+
+        const jobType = argSettingOrDefault(
+            commandLineArguments,
+            jobTypes,
+            jobTypes.BA
+        );
+
+        const output = {};
+
+        output.documentNameText = match({
+            [documentTypes.CV]: "CV",
+            default: "Resume"
+        })(documentType);
+
+        output.documentNameLatex = match({
+            [documentTypes.CV]: "CURRICULUM VITAE",
+            default: String.raw`R\'{E}SUM\'{E}`
+        })(documentType);
+
+        return output;
+    })();
+
+    const templatePath = "./resources/template.mustache.tex";
+    const outputPath = `./output/${data.documentNameText} of Benjamin Glitsos.tex`;
+    const mustacheSettings = metadata.mustache.customTags;
+
+    const template = fs.readFileSync(templatePath, "utf8");
 
     fs.writeFileSync(
         outputPath,
-        Mustache.render(template, data, {}, metadata.mustache.customTags)
+        Mustache.render(template, data, {}, mustacheSettings)
     );
 
     console.log(logNumber() + chalk`Wrote to {blue ${outputPath}}`);
--- src/utilities.js
+++ src/utilities.js
@@ -2,6 +2,8 @@ const chalk = require("chalk");
 const { STATE } = require("./state");
 const { metadata } = require("./metadata");
 
+module.exports.identity = x => x;
+
 module.exports.logNumber = () =>
     chalk`{magenta.bold ${
         metadata.general.programName
